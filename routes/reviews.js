4f3d28962f2b1d0a830d92924170db60ddb2d9df
jaehoshin123 <fluffyck@naver.com>
2026-01-28 14:00:12 +0900

    added flash

--- routes/reviews.js
+++ routes/reviews.js
@@ -18,20 +18,22 @@ const validateReview = (req, res, next) => {
     }
 }
 
-router.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+router.post('/', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     console.log(campground);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash('success', 'Created new review!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-router.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Successfully deleted review');
     res.redirect(`/campgrounds/${id}`);
 }));
 
